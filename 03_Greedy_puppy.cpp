dd005775a1d1dc8e64d98606d9912fe76959f172
Raaz <73809433+Raaz16@users.noreply.github.com>
2021-10-11 21:54:56 -0700

    Update 03_Greedy_puppy.cpp

--- 03_Greedy_puppy.cpp
+++ 03_Greedy_puppy.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 int main()
 {
-    int t, n, k;
+    int t;
     cin >> t;
     while (t--)
     {
+       int n, k;
+        int r = 0;
         cin >> n >> k;
-        int r = n % k;
+        for (int i = 1; i <= k; i++)
+        {
+            if (n % i > r)
+            {
+                r = n % i;
+            }
+        }
         cout << r << endl;
     }
+    
+    return 0;
 }
